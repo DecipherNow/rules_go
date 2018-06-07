93e8b6ecc306fecc6bd25259ac40a50f1af2944b
Charles Strahan <charles@cstrahan.com>
2018-06-07 15:28:32 -0400

    cgo hacks for gm-proxy (envoy bindings)
    (cherry picked from commit 096998c6b4ba795dce14ff18941016671381da2d)

--- go/tools/builders/cgo.go
+++ go/tools/builders/cgo.go
@@ -28,8 +28,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
+
+var linkLine = regexp.MustCompile(`^//go:cgo_ldflag ".*/envoy/.*\.(o|lo|a)"$`)
 
 func cgo(args []string) error {
 	args, err := readParamsFiles(args)
@@ -211,6 +214,35 @@ func cgo(args []string) error {
 			return err
 		}
 	}
+
+	// Begin hack
+	// 1. Determine path to _cgo_gotypes.go
+	for objDir, _ := range objDirs {
+		gotypesPath := filepath.Join(objDir, "_cgo_gotypes.go")
+
+		dat, err := ioutil.ReadFile(gotypesPath)
+		if err != nil {
+			return err
+		}
+
+		// 2. Add --{,no-}whole-archive around each linked object/archive
+		lines := []string{}
+		for _, line := range strings.Split(string(dat), "\n") {
+			if linkLine.MatchString(line) {
+				lines = append(lines, `//go:cgo_ldflag "-Wl,--whole-archive"`)
+				lines = append(lines, line)
+				lines = append(lines, `//go:cgo_ldflag "-Wl,--no-whole-archive"`)
+			} else {
+				lines = append(lines, line)
+			}
+		}
+
+		err = ioutil.WriteFile(gotypesPath, []byte(strings.Join(lines, "\n")), 0555)
+		if err != nil {
+			return err
+		}
+	}
+	// End hack
 
 	return nil
 }
